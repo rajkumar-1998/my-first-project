f6d1b86ec83db920e749f02145c00f2696ac896e
rajkumar <rajkumarofficialmail11@gmail.com>
2022-12-27 10:58:18 +0530

    first java

---
+++ com.test.java/src/com/test/java/Sample.java
@@ -0,0 +1,18 @@
+package com.test.java;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int a=11;
+		
+		if(a%2==0) {
+			System.out.println(a+" is even number");
+		}
+		else {
+			System.out.println(a+" is odd number");
+		}
+
+	}
+
+}
